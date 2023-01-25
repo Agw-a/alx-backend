04d89f298f4bc602f80fe746e35626f3127837eb
Agw-a <lizagwa7@gmail.com>
2023-01-25 16:57:43 +0300

    fix if statement

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -10,7 +10,7 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         '''Add an item to the cache
         '''
-        if key or item is None:
+        if key is None or item is None:
             return
         self.cache_data[key] = item
 
