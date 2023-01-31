3748f26dfd6d28a9594c75a3cd01f96aec9bc265
Agw-a <lizagwa7@gmail.com>
2023-01-31 20:03:00 +0300

    Task 3

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+'''A basic flask app
+'''
+from flask_babel import Babel, gettext
+from flask import Flask, render_template
+from requests import request
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    '''configure local settings
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    '''get locale from webpage
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def get_home():
+    ''' Define defaults and renders
+    '''
+    return render_template('3-index.html')
