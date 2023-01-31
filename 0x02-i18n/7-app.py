fdd132e90cd3a86924f667775d7707237e884c9a
Agw-a <lizagwa7@gmail.com>
2023-01-31 22:21:07 +0300

    setting up

---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+'''A basic flask app
+'''
+from flask_babel import Babel
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+class Config():
+    '''configure local settings
+    '''
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    '''get locale from webpage
+    '''
+    cues = request.query_string.decode('utf-8').split('&')
+    cue = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        cues,
+    ))
+    if 'locale' in cue:
+        if cue['locale'] in app.config["LANGUAGES"]:
+            return cue['locale']
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_home():
+    ''' Define defaults and renders
+    '''
+    return render_template('4-index.html')
