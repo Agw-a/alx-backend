cdb224cd2a36ca96c3ca3d10500f3888cb2dacbd
Agw-a <lizagwa7@gmail.com>
2023-01-31 18:49:11 +0300

    Task 2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 '''A basic flask app
 '''
-from flask import Flask, render_template
 from flask_babel import Babel
+from flask import Flask, render_template
+from requests import request
 
 app = Flask(__name__)
 babel = Babel(app)
 
 
 class Config():
-    '''configurs babel language
+    '''configure local settings
     '''
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
@@ -19,8 +20,15 @@ class Config():
 app.config.from_object(Config)
 
 
+@babel.localeselector
+def get_locale():
+    '''get locale from webpage
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def get_home():
     ''' Define defaults and renders
     '''
-    return render_template('1-index.html')
+    return render_template('2-index.html')
